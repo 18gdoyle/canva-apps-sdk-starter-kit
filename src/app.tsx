1a9a987a6693b87bd37680cfa83d281d578f5eda
Greg Doyle <83316865+18gdoyle@users.noreply.github.com>
2023-10-06 13:57:27 -0400

    added a working input text box

--- src/app.tsx
+++ src/app.tsx
@@ -1,34 +1,70 @@
 import { Button, Rows, Text } from "@canva/app-ui-kit";
 import { addNativeElement } from "@canva/design";
+import { selection, SelectionEvent } from "@canva/preview/design";
 import * as React from "react";
 import styles from "styles/components.css";
 
 export const App = () => {
-  const onClick = () => {
+  const [event, setEvent] = React.useState<
+    SelectionEvent<"text"> | undefined
+  >();
+
+  React.useEffect(() => {
+    selection.registerOnChange({
+      scope: "text",
+      onChange: async (event) => {
+        setEvent(event);
+      },
+    });
+  }, []);
+
+  const isElementSelected = event && event.count > 0;
+
+  const createTextBox = () => {
     addNativeElement({
       type: "TEXT",
       children: ["Hello world!"],
     });
   };
 
+  const updateTextBox = async (e: { target: { value: any; }; }) => {
+    if (!event || !isElementSelected) {
+      return;
+    }
+
+    await selection.setContent(event, (value) => {
+      return {
+        text: e.target.value,
+      };
+    });
+  }
+
   return (
     <div className={styles.scrollContainer}>
       <Rows spacing="2u">
-        <Text>
-          To make changes to this app, edit the{" "}
-          <code className={styles.code}>src/app.tsx</code> file, then close and
-          reopen the app in the editor to preview the changes.
-        </Text>
-        <Button variant="primary" onClick={onClick} stretch>
-          Do something cool
+        <Button variant="primary" onClick={createTextBox} stretch>
+          Create a New Text Box
         </Button>
-        <form>
-          <label>
-            Name:
-            <input type="text" name="name" />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
+
+        {(() => {
+          if (isElementSelected) {
+            return (
+              <div>
+                Enter text
+                <br></br>
+                <input type="text" name="name" onChange={updateTextBox} />
+              </div>
+            )
+          } else {
+            return (
+              <div>
+                -- OR --
+                <br></br>
+                Select a text box on the canvas to edit it
+              </div>
+            )
+          }
+        })()}
       </Rows>
     </div>
   );
